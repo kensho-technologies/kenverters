74b377d04c84ba89af05b1eb152e2c39f47d916d
Valerie Faucon-Morin <valerie.fauconmorin@kensho.com>
2024-06-24 10:33:42 -0400

    found mistake

--- kensho_kenverters/tests/test_changelog_updated.py
+++ kensho_kenverters/tests/test_changelog_updated.py
@@ -5,7 +5,7 @@ import unittest
 from importlib import metadata, resources
 
 _version_pattern = re.compile(r"## v(\d+\.\d+\.\d+)")
-package_name = "kenverters"
+package_name = "kensho_kenverters"
 
 
 class ChangelogTests(unittest.TestCase):
