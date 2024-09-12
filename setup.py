65dcf5701ea70d0f246e7222152648df014b207c
Valerie Faucon-Morin <valerie.fauconmorin@kensho.com>
2024-09-12 08:58:15 -0400

    fix license

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
 
-    'License :: OSI Approved :: Apache License',
+    'License :: OSI Approved :: Apache Software License',
 
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
