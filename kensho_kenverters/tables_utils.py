e347e229305f67736cfacf478a80891fa386d4f1
Libin Liang <libin.liang@kensho.com>
2025-10-13 17:05:38 -0400

    debug

--- kensho_kenverters/tables_utils.py
+++ kensho_kenverters/tables_utils.py
@@ -174,7 +174,8 @@ def get_projected_row_header_row_indexes(
             if row_index not in projected_row_header_row_indexes:
                 projected_row_header_row_indexes.append(row_index)
 
-    return projected_row_header_row_indexes.sort()
+    projected_row_header_row_indexes.sort()
+    return projected_row_header_row_indexes
 
 
 def _verify_column_header_row(content_string_list: list[str]) -> bool:
